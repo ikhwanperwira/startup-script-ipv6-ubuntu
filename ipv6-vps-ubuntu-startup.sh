31e22354084012f45c11aeefc856ef07bac3fd36
wawan-ikhwan <ikhwanperwira@gmail.com>
2023-11-04 20:04:51 +0700

    add rclone conf

--- ipv6-vps-ubuntu-startup.sh
+++ ipv6-vps-ubuntu-startup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-apt update -y
-apt upgrade -y
-apt install screen -y
+# argument $1 diisi dengan konfigurasi rclone.conf
+
+export DEBIAN_FRONTEND=noninteractive
+apt -y update
+apt -y upgrade
+apt -y install screen
 echo 'caption always "%{= kc}Screen session on %H (system load: %l)%-28=%{= .m}%D %d.%m.%Y %0c"' >> /root/.screenrc
 echo 'termcapinfo xterm* ti@:te@' >> /root/.screenrc
 echo "screen -DRR" >> /root/.profile
@@ -10,5 +13,12 @@ echo "screen -DRR" >> /root/.profile
 wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh
 chmod +x /root/menu.sh
 echo "" | /root/menu.sh 4
+exec bash
 
 wget -N https://github.com/rclone/rclone/releases/download/v1.64.2/rclone-v1.64.2-linux-amd64.zip
+apt -y install unzip
+unzip rclone-v1.64.2-linux-amd64.zip
+cp rclone-v1.64.2-linux-amd64/rclone /bin
+mkdir /root/.config
+mkdir /root/.config/rclone
+cp $1 /root/.config/rclone
