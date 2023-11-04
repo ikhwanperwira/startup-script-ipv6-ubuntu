82eb8726bd680e1ed1f55e550820fc8dc992e9fe
wawan-ikhwan <ikhwanperwira@gmail.com>
2023-11-04 20:45:04 +0700

    foo

--- ipv6-vps-ubuntu-startup.sh
+++ ipv6-vps-ubuntu-startup.sh
@@ -17,12 +17,10 @@ echo "screen -DRR" >> /root/.profile
 # install warp
 wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh
 chmod +x /root/menu.sh
-yes "" | /root/menu.sh 4 &&
-
-echo ""
+yes "" | /root/menu.sh 4
 
 # install rclone
-wget -N https://github.com/rclone/rclone/releases/download/v1.64.2/rclone-v1.64.2-linux-amd64.zip
+yes "" | wget -N https://github.com/rclone/rclone/releases/download/v1.64.2/rclone-v1.64.2-linux-amd64.zip
 apt -y install unzip
 unzip /root/rclone-v1.64.2-linux-amd64.zip
 cp /root/rclone-v1.64.2-linux-amd64/rclone /bin
