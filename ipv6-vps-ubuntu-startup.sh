19b39a65423aa2b2fbabe46d51793727ca58b82d
wawan-ikhwan <ikhwanperwira@gmail.com>
2023-11-05 13:27:59 +0700

    create fuse wget

--- ipv6-vps-ubuntu-startup.sh
+++ ipv6-vps-ubuntu-startup.sh
@@ -65,8 +65,8 @@ cp /root/rclone.conf /root/.config/rclone
 # creating fuse device
 apt -y install fuse3
 if hostnamectl | grep -q openvz && [ ! -e /dev/fuse ]; then
-  # wget create-fuse-node.sh
-  # wget create-fuse-node.service
+  wget -N https://raw.githubusercontent.com/wawan-ikhwan/startup-script-ipv6-ubuntu/main/create-fuse-node.sh
+  wget -N https://raw.githubusercontent.com/wawan-ikhwan/startup-script-ipv6-ubuntu/main/create-fuse-node.service
   chmod +x /root/create-fuse-node.sh
   mv /root/create-fuse-node.sh /usr/local/bin/
   systemctl daemon-reload
