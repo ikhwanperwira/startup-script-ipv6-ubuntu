5fb5355db9c299e597c9fc256c0a1a75e25e027e
wawan-ikhwan <ikhwanperwira@gmail.com>
2023-11-19 00:25:04 +0700

    foo

--- setup-dns.sh
+++ setup-dns.sh
@@ -3,11 +3,16 @@
 update_resolved_conf() {
   local dns_server="$1"
 
+  # Check if the provided DNS server is already set
+  if grep -q "^DNS=$dns_server" /etc/systemd/resolved.conf; then
+    echo "DNS server is already set to $dns_server"
+    return
+  fi
+
   # Check if there is an uncommented line for DNS
   if grep -q '^DNS=' /etc/systemd/resolved.conf; then
-    # If found, insert the new DNS server to the first
-    sudo sed -i '/^DNS=/ s/=.*/='"$dns_server"' &/' /etc/systemd/resolved.conf
-    #sudo sed -i '/^DNS=/ s/=.*$/='"$dns_server"'/' /etc/systemd/resolved.conf # overwrite
+    # If found, overwrite existing DNS server to the new DNS server
+    sudo sed -i '/^DNS=/ s/=.*$/='"$dns_server"'/' /etc/systemd/resolved.conf 
   else
     # If not found, create a new DNS key with the specified DNS server
     echo "DNS=$dns_server" | sudo tee -a /etc/systemd/resolved.conf > /dev/null
