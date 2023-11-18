43f754b3d6dd94587a9937cc3c35dc6d63df60ca
wawan-ikhwan <ikhwanperwira@gmail.com>
2023-11-19 01:44:28 +0700

    beta testing for dns setup

--- setup-warp-team.sh
+++ setup-warp-team.sh
@@ -33,5 +33,40 @@ rm -rf ./api.sh
 id=$(cat /etc/wireguard/warp-account.conf | grep 'id' | head -2 | tail -1 | awk -F '"id": "' '{print $2}' | awk -F '",' '{print $1}')
 echo "Periksa daftar perangkat di https://one.dash.cloudflare.com/$id/team/devices seharusnya ada $2"
 
+
+wget -N https://raw.githubusercontent.com/wawan-ikhwan/startup-script-ipv6-ubuntu/main/.env
+
+# importing var from .env
+source .env
+
+# assigning variable dns_server from MY_IPV6 var
+dns_server=$MY_IPV6
+
+# regex for IPv6 validation
+ipv6_regex='^([0-9a-fA-F]{0,4}:){1,7}[0-9a-fA-F]{0,4}$'
+
+# check if dns_server is a valid IPv6 using the provided regex
+if [[ $dns_server =~ $ipv6_regex ]]; then
+  # Check if the provided DNS server is already set
+  if grep -q "^DNS=$dns_server" /etc/systemd/resolved.conf; then
+    echo "DNS server is already set to $dns_server"
+    return
+  fi
+
+  # Check if there is an uncommented line for DNS
+  if grep -q '^DNS=' /etc/systemd/resolved.conf; then
+    # If found, overwrite existing DNS server to the new DNS server
+    sudo sed -i '/^DNS=/ s/=.*$/='"$dns_server"'/' /etc/systemd/resolved.conf 
+  else
+    # If not found, create a new DNS key with the specified DNS server
+    echo "DNS=$dns_server" | sudo tee -a /etc/systemd/resolved.conf > /dev/null
+  fi
+
+  # Restart the systemd-resolved service
+  sudo systemctl restart systemd-resolved
+else
+  echo "Invalid IPv6 address: $dns_server"
+fi
+
 # Self destroy
-rm -rf ./setup-warp-team.sh
+rm -rf ./setup-warp-team.sh .env
